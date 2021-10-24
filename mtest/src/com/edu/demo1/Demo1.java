4b7348a4c4ca746f868a1d8db681d61e9d55e9b4
lvyanze <lvyanze1@sina.com>
2021-10-24 21:22:18 +0800

    update1

--- mtest/src/com/edu/demo1/Demo1.java
+++ mtest/src/com/edu/demo1/Demo1.java
@@ -5,5 +5,6 @@ public class Demo1 {
     public  static void main(String[] args){
       System.out.println("hello world");
         System.out.println("update1");
+        System.out.println("update2");
     }
 }
